9f718b8bfa8e7651847a11eadc6468262507b6bc
Oleg Kachinskyi <logansoleg@gmail.com>
2023-02-12 20:56:25 +0200

    Add express.js usage

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
-import http from 'http';
+import express from 'express';
 import { createClient } from 'redis';
 
 const client = createClient({
-    url: 'redis://redis-service:6379',
+    url: process.env.REDIS_URL,
 });
 
 client.on('error', err => console.log('Redis Client Error', err));
 
-const server = http.createServer(async (req, res) => {
-    console.log('Incomming request');
-    console.log(process.env.TEST_ENV_VARIABLE || '"TEST_ENV_VARIABLE" is not defined.');
-    console.log(process.env.SECRET_USERNAME || '"SECRET_USERNAME" is not defined.');
-    console.log(process.env.SECRET_PASSWORD || '"SECRET_PASSWORD" is not defined.');
+const app = express();
+app.use(express.json());
+
+app.get('/liveness', async (req, res) => {
+    const requestCount = parseInt(await client.get(REQUEST_COUNT) || 0);
+
+    const { headers } = req;
+    const k8sHeaders = Object.keys(headers).filter(key => key.match(/k8s\-.*/g));
+    console.log(
+        `[${new Date().toISOString()}]`,
+        '/liveness called',
+        k8sHeaders.reduce((acc, key) => ({ ...acc, [key]: headers[key] }), {}),
+        `Request count: ${requestCount}`,
+    );
+
+    if (requestCount !== 0 && requestCount % 10 === 0) {
+        res.status(400).json({ error: 'Testing probes /liveness' });
+        return;
+    }
+
+    res.json({});
+    return;
+});
+
+app.get('/health', async (req, res) => {
+    const requestCount = parseInt(await client.get(REQUEST_COUNT) || 0);
+
+    const { headers } = req;
+    const k8sHeaders = Object.keys(headers).filter(key => key.match(/k8s\-.*/g));
+    console.log(
+        `[${new Date().toISOString()}]`,
+        '/health called',
+        k8sHeaders.reduce((acc, key) => ({ ...acc, [key]: headers[key] }), {}),
+        `Request count: ${requestCount}`,
+    );
+
+    if (requestCount !== 0 && requestCount % 10 === 0) {
+        res.status(400).json({ error: 'Testing probes /health' });
+        return;
+    }
+
+    res.json({});
+    return;
+});
+
+const REQUEST_COUNT = 'request-count';
+
+app.get('/*', async (req, res) => {
+    const requestCount = await client.get(REQUEST_COUNT) || 0;
+    const requestNum = parseInt(requestCount) + 1;
+
+    await client.set(REQUEST_COUNT, requestNum);
+
+    console.log('Incomming request', requestNum);
     const { headers } = req;
 
     // Write all headers to Redis
     const promises = Object.keys(headers).map(key => client.set(key, headers[key]));
     await Promise.all(promises);
 
-    res.write(`Requested path: ${req.url}\n`);
-    res.write(`Requested headers: ${JSON.stringify(req.headers)}\n`);
-
+    const envVars = ['TEST_ENV_VARIABLE', 'SECRET_USERNAME', 'SECRET_PASSWORD'];
+    const data = {
+        path: req.url,
+        headers: headers,
+        env: envVars
+            .reduce((acc, key) => ({ ...acc, [key]: process.env[key] || `"${key}" is not defined.` }), {}),
+    };
     if (headers['redis-key']) {
         const value = await client.get(headers['redis-key']);
-        res.write(`Redis value: ${value}\n`);
-    }
-    res.end();
-});
 
+        data.redis = { [headers['redis-key']]: value };
+    }
+    res.setHeader('x-request-count', requestNum);
+    res.json(data);
+    return;
+});
 
 process.on('SIGINT', () => {
     client.disconnect();
@@ -37,8 +92,10 @@ process.on('SIGINT', () => {
 const port = parseInt(process.env.APP_PORT) || 3000;
 
 client.connect()
-    .then(() => {
-        server.listen(port, () => {
+    .then(async () => {
+        await client.set(REQUEST_COUNT, 0);
+
+        app.listen(port, () => {
             console.log(`Server has started on port "${port}"`);
         });
     })
